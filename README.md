3c013dc3fb9174223a2b41d2c33730c895366545
Vasileios Moratis <vassi07@gmail.com>
2026-04-17 21:23:44 +0200

    Add README and metadata to homepage

---
+++ README.md
@@ -0,0 +1,3 @@
+# this is a Next.js E-commerce store project .
+
+# Demonstrating features of Postgres.js and Next.js Server Components.
---
+++ app/page.js
@@ -0,0 +1,14 @@
+
+export const metadata = {
+  title: 'Home',
+  description: 'This is the home page of our Next.js application.',
+};
+
+
+
+export default function Home() {
+  return (
+    <h1>This is the home page</h1>
+
+  );
+}
